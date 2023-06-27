ab6e4dfd783c2b1ecd719d05a617b0bc10bb7535
RealSopaper <luis.ugartechea.enp5.804@gmail.com>
2023-06-26 20:27:54 -0600

    modificaciones final dados

--- dp/dados.c
+++ dp/dados.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 
 int sumaDados(int numDados, int n) {
-    // Crear una matriz para almacenar los subproblemas
-    int info[numDados + 1][n + 1];
-    // Caso base: si no hay dados, no hay suma posible
+    int info[numDados + 1][n + 1]; // crea una matriz para almacenar los subproblemas
     info[0][0] = 1;
 
-    // Calcular las soluciones a los subproblemas
     for (int i = 1; i <= numDados; i++) {
         for (int j = 1; j <= n; j++) {
-            // Verificar si el dado actual puede contribuir a la suma actual
-            if (j <= 6) {
+            if (j <= 6) { // verifica si el dado actual puede contribuir a la suma actual
                 info[i][j] = info[i - 1][j];
             }
-            // Sumar las soluciones anteriores para obtener la solución actual
-            info[i][j] += (j > 6) ? info[i][j - 1] : 0;
+            info[i][j] += (j > 6) ? info[i][j - 1] : 0; // Suma las soluciones anteriores para obtener el resultado
         }
-    }
+    } // este for tiene como objetivo calcular las soluciones a los subproblemas
 
-    // Devolver el resultado final
-    return info[numDados][n];
+    return info[numDados][n]; //devuelve el resultado final
 }
 
 int main() {
@@ -30,3 +24,4 @@ int main() {
 
     return 0;
 }
+
